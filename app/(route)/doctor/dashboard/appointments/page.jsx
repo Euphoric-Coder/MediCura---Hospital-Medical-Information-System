761118429e00537b62ec118ea74c0949359f7690
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2025-10-14 22:26:34 +0530

    Refactor appointments page to render DoctorAppointments component and utilize router for navigation

--- app/(route)/doctor/dashboard/appointments/page.jsx
+++ app/(route)/doctor/dashboard/appointments/page.jsx
@@ -1,13 +1,18 @@
 "use client";
 
+import DoctorAppointments from "@/components/Doctor/DoctorAppointments";
 import { useDoctor } from "@/contexts/DoctorContext";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const page = () => {
+  const router = useRouter();
   const { doctorData } = useDoctor();
   console.log("Doctor Data in Page:", doctorData);
 
-  return <div>page</div>;
+  return (
+    <DoctorAppointments onBack={() => router.back()} doctorData={doctorData} />
+  );
 };
 
 export default page;
