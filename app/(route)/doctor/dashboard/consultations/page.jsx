dfa93d37d4e5e1b43fea5ef5c4b5766c30da8f8e
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2025-10-14 22:33:39 +0530

    Refactor consultations page to render DoctorConsultations component and implement navigation; Working on the consultation of the doctor UI design

--- app/(route)/doctor/dashboard/consultations/page.jsx
+++ app/(route)/doctor/dashboard/consultations/page.jsx
@@ -1,13 +1,18 @@
 "use client";
 
+import DoctorConsultations from "@/components/Doctor/DoctorConsultations";
 import { useDoctor } from "@/contexts/DoctorContext";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const page = () => {
+  const router = useRouter();
   const { doctorData } = useDoctor();
   console.log("Doctor Data in Page:", doctorData);
 
-  return <div>page</div>;
+  return (
+    <DoctorConsultations onBack={() => router.back()} doctorData={doctorData} />
+  );
 };
 
 export default page;
